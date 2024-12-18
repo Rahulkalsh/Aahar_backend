f17b5801520bf3da2bc8307f19ecde4f3d114f8d
Rahulkalsh <108459884+Rahulkalsh@users.noreply.github.com>
2024-12-18 14:10:06 +0530

    Update Auth.js

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -33,7 +33,6 @@ router.post('/createuser',
             });
             res.json({ success: true, user: newUser });
         } catch (error) {
-            console.error(error);
             res.json({ success: false, error: error.message || "An error occurred" });
         }
     });
@@ -65,7 +64,6 @@ router.post('/createuser',
             const authToken = jwt.sign(data,jwtSecret)
             return res.json({success: true,authToken:authToken });
         } catch (error) {
-            console.error(error);
             res.json({ success: false, error: error.message || "An error occurred" });
         }
     });
@@ -87,16 +85,12 @@ router.post('/getlocation', async (req, res) => {
     try {
         let lat = req.body.latlong.lat
         let long = req.body.latlong.long
-        console.log(lat, long)
         let location = await axios
             .get("https://api.opencagedata.com/geocode/v1/json?q=" + lat + "+" + long + "&key=74c89b3be64946ac96d777d08b878d43")
             .then(async res => {
-                // console.log(`statusCode: ${res.status}`)
-                console.log(res.data.results)
                 // let response = stringify(res)
                 // response = await JSON.parse(response)
                 let response = res.data.results[0].components;
-                console.log(response)
                 let { village, county, state_district, state, postcode } = response
                 return String(village + "," + county + "," + state_district + "," + state + "\n" + postcode)
             })
@@ -106,14 +100,11 @@ router.post('/getlocation', async (req, res) => {
         res.send({ location })
 
     } catch (error) {
-        console.error(error.message)
         res.send("Server Error")
 
     }
 });
 router.post("/foodData",(req,res)=>{
-
-    console.log(global.food_items,global.foodCategory);
 
     try {
         res.json([global.food_items,global.foodCategory]);
@@ -127,15 +118,11 @@ router.post("/foodData",(req,res)=>{
 router.post('/orderData', async (req, res) => {
     let data = req.body.order_data
     await data.splice(0,0,{Order_date:req.body.order_date})
-    console.log("1231242343242354",req.body.email)
 
     //if email not exisitng in db then create: else: InsertMany()
     let eId = await Order.findOne({ 'email': req.body.email })    
-    console.log(eId)
     if (eId===null) {
         try {
-            console.log(data)
-            console.log("1231242343242354",req.body.email)
             await Order.create({
                 email: req.body.email,
                 order_data:[data]
@@ -163,9 +150,7 @@ router.post('/orderData', async (req, res) => {
 })
 router.post('/myOrderData', async (req, res) => {
     try {
-        console.log(req.body.email)
         let eId = await Order.findOne({ 'email': req.body.email })
-        //console.log(eId)
         res.json({orderData:eId})
     } catch (error) {
         res.send("Error",error.message)
